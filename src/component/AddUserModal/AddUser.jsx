2894eabe37299db92c47e737b8c76d7b8224a262
Aryan <aryanrot234@gmail.com>
2023-09-03 21:20:02 +0530

    fiexed error

--- src/component/AddUserModal/AddUser.jsx
+++ src/component/AddUserModal/AddUser.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { FaUserPlus } from "react-icons/fa";
-import "./Adduser.css";
+import "./AddUser.css";
 
 const AddUser = () => {
   return (
-    <div
-      className="component"
-    >
-      <FaUserPlus size={30}/>
+    <div className="component">
+      <FaUserPlus size={30} />
       <h5>Add New User</h5>
       {/* <br /> */}
     </div>
